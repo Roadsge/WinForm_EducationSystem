85b64f3ba7200d6ff7d8ac091194a710657cac7f
TAO <goodprintf@qq.com>
2026-04-02 22:57:33 +0800

    Add base classes.

--- MaAnShanTeachersCollege/WinForm_EducationSystem.cs
+++ MaAnShanTeachersCollege/WinForm_EducationSystem.cs
@@ -8,6 +8,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,12 +18,10 @@ namespace EducationSystem
     public delegate void MyDelegate(string messgee);
     public partial class WinForm_EducationSystem : Form
     {
+        public static int UserID = 0;
         public WinForm_EducationSystem()
         {
             InitializeComponent();
-
-
-
         }
 
         public void ShowMessage(string message)
@@ -45,17 +44,71 @@ namespace EducationSystem
             SettingPage settingPage = new SettingPage();
             panelContent.Controls.Add(settingPage);
         }
+        /// <summary>
+        /// 根据字符串获取相应的图片资源
+        /// </summary>
+        /// <param name="name">图片名称</param>
+        /// <returns></returns>
+        public Bitmap GetBitmapFromResx(string name)
+        {
+            Type type = typeof(Resources);
+            //用反射获取
+            PropertyInfo info = type.GetProperty(name, BindingFlags.Static | BindingFlags.NonPublic);
+            if(info!=null&&info.PropertyType==typeof(Bitmap))
+            {
+                return (Bitmap)info.GetValue(null);
+            }
+            return null;
+        }
+        private void MenuJump_Click(object sender,EventArgs e)
+        {
+            panelContent.Controls.Clear();
+
+        }
+        /// <summary>
+        /// Loads a user control by name and displays it in the content panel.
+        /// </summary>
+        /// <remarks>Displays a message box if the control type is not found or cannot be
+        /// instantiated.</remarks>
+        /// <param name="pageName">The name of the user control to load.</param>
+        public void LoadPage(string pageName)
+        {
+            var assemly = Assembly.GetExecutingAssembly();
+            string fullTypeName = $"{this.GetType().Namespace}.Pages.{pageName}";
+            Type type = assemly.GetType(fullTypeName);
+            if(type==null)
+            {
+                MessageBox.Show($"未找到相应控件{fullTypeName}" );
+                return;
+            }
+            var page = Activator.CreateInstance(type) as UserControl;
+            if(page==null)
+            {
+                MessageBox.Show("无法创建页面实例");
+                return;
+            }
+            panelContent.Controls.Clear();
+            page.Dock = DockStyle.Fill;
+            panelContent.Controls.Add(page);
+
+        }
 
         private void EducationSystem_Load(object sender, EventArgs e)
         {
             ////加载菜单
-            //string path = @"C:用户\sky\桌面\MyFile.txt";
-            //string file = File.ReadAllText(path);
-            //string[] id = file.Split(',');
-            //MenuUC menuUC1 = new MenuUC();
-            //menuUC1.MenuText = "首页";
-            //menuUC1.MenuImage = Resources.home;
-            //flPanelMenu.Controls.Add(menuUC1);
+            string path = @"C:\Users\sky\Desktop\MyFile.txt";
+            string file = File.ReadAllText(path);
+            string[] id = file.Split(',');
+            MenuUC menuUC1 = new MenuUC();
+            menuUC1.MenuText = "首页";
+            menuUC1.MenuImage = Resources.home;
+            menuUC1.LabelClick+=(newSender,newE)=>
+            {
+                panelContent.Controls.Clear();
+                HomePage home = new HomePage();
+                panelContent.Controls.Add(home);
+            }
+            flPanelMenu.Controls.Add(menuUC1);
 
         }
     }
